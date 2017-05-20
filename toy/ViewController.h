7904f31615b80064bfde291ffcdb294679d7c1f3
lisply <louiely@qq.com>
2017-05-20 16:01:23 +0800

    init

---
+++ toy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  toy
+//
+//  Created by LuLouie on 2017/5/20.
+//  Copyright © 2017年 LuLouie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ toy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  toy
+//
+//  Created by LuLouie on 2017/5/20.
+//  Copyright © 2017年 LuLouie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
